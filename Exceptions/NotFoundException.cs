941808210a4511d1200748499238dd43103349dd
Azizjon <azizaliev2337@gmail.com>
2024-02-24 08:54:04 +0500

    add exception not found for entity

---
+++ Exceptions/NotFoundException.cs
@@ -0,0 +1,8 @@
+namespace BlogAPI.Exceptions;
+
+public class NotFoundException: Exception
+{
+    public NotFoundException(string name, object key) : base($"Entity \"{name}\" ({key}) not found.")
+    {
+    }
+}
